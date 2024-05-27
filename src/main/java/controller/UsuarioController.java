0ee1e7d8785db48b8550a6010d4f70b4dfdfeb7a
JoseDavid721 <117630884+JoseDavid721@users.noreply.github.com>
2024-05-26 22:27:13 -0500

    Update UsuarioController.java

--- src/main/java/controller/UsuarioController.java
+++ src/main/java/controller/UsuarioController.java
@@ -1,14 +1,49 @@
-package Controller;
-import Modulo.Usuario;
+package controller;
 import View.UsuarioView;
+import Usuario.UsuarioDao;
 public class UsuarioController {
     private Usuario usuarioModel;
     private UsuarioView usuarioView;
+    private UsuarioDao usuarioDAO;
 
     public UsuarioController(Usuario usuarioModel, UsuarioView usuarioView) {
         this.usuarioModel = usuarioModel;
         this.usuarioView = usuarioView;
     }
+    public UsuarioController(UsuarioDao usuarioDAO) {
+        this.usuarioDAO = usuarioDAO;
+    }
+    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Entró al DoGet");
+        String a=req.getParameter("accion");
+
+        switch(a){
+            case "abrirForm":
+                abrirForm(req,resp);
+            break;
+            case "listar":
+                listar(req,resp);
+            break;
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Entró al DoPost");
+        String a=req.getParameter("accion");
+
+        switch(a){
+            case "add":
+                add(req,resp);
+            break;
+        }
+    }
+
+public void actualizarUsuario(String id, String nuevoNombre, String nuevoEmail) {
+    usuarioDAO.actualizarUsuario(id, nuevoNombre, nuevoEmail);
+}
 
     // Métodos para actualizar y obtener información del usuario
     // ...
@@ -16,4 +51,7 @@ public class UsuarioController {
     public void actualizarVista() {
         usuarioView.mostrarUsuario(usuarioModel);
     }
+    public Usuario obtenerUsuarioPorId(int id) {
+        return usuarioDAO.obtenerUsuarioPorId(id);
+    }
 }
